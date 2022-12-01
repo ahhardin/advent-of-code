58ed9c90a3f2700475a0cd9f16be55296a0c9a37
Ashley Hardin <33456381+ahhardin@users.noreply.github.com>
2022-12-01 14:05:50 -0500

    add comment

--- 2022/01.py
+++ 2022/01.py
@@ -1,6 +1,7 @@
 import requests
 from secrets import SESSION
 
+# part 1
 response = requests.get('https://adventofcode.com/2022/day/1/input', headers={'Cookie': SESSION})
 split_data = response.content.decode('utf-8').strip().split('\n\n')
 data = [d.split('\n') for d in split_data]
@@ -8,5 +9,6 @@ calorie_sum = [sum(map(int, cals)) for cals in data]
 calorie_sum.sort(reverse=True)
 top_calories = calorie_sum[0]
 print(top_calories)
+# part 2
 top_three = sum(calorie_sum[:3])
 print(top_three)
