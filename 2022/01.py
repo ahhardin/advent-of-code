b17e2a99300592d978dc028aded885070ee8140c
Ashley Hardin <33456381+ahhardin@users.noreply.github.com>
2022-12-01 13:38:45 -0500

    try again

---
+++ 2022/01.py
@@ -0,0 +1,12 @@
+import requests
+from secrets import SESSION
+
+response = requests.get('https://adventofcode.com/2022/day/1/input', headers={'Cookie': SESSION})
+split_data = response.content.decode('utf-8').strip().split('\n\n')
+data = [d.split('\n') for d in split_data]
+calorie_sum = [sum(map(int, cals)) for cals in data]
+calorie_sum.sort(reverse=True)
+top_calories = calorie_sum[0]
+print(top_calories)
+top_three = sum(calorie_sum[:3])
+print(top_three)
