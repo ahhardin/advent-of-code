09dfe4a88ec0427120567977907bb2167d59d0db
Ashley Hardin <33456381+ahhardin@users.noreply.github.com>
2022-12-04 17:06:08 -0500

    oops

--- 2022/04.py
+++ 2022/04.py
@@ -7,7 +7,7 @@ data = response.content.decode('utf-8').strip().split('\n')
 full_overlaps = 0
 partial_overlaps = 0
 for d in data:
-    matches = re.search("(?P<e1a>[0-9]+)-(?P<e1b>[0-9]+),(?P<e2a>[0-9]+)-(?P<e2b>[[0-9]+)", d)
+    matches = re.search("(?P<e1a>[0-9]+)-(?P<e1b>[0-9]+),(?P<e2a>[0-9]+)-(?P<e2b>[0-9]+)", d)
     e1a = int(matches.group('e1a'))
     e1b = int(matches.group('e1b'))
     e2a = int(matches.group('e2a'))
