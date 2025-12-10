1edc3b19204d479cce4700a317529712f41deac5
Ashley Hardin <33456381+ahhardin@users.noreply.github.com>
2025-12-09 21:08:51 -0500

    day 9

---
+++ years/2025/09.py
@@ -0,0 +1,68 @@
+from years.process import *
+from shapely import Polygon
+
+test_input = """
+7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3
+"""
+real_input = get_response(day=9, year=2025)
+
+def process_input_into_coords(input_data):
+    return [tuple((int(line)) for line in di.split(",")) for di in input_data.strip().split("\n")]
+
+def get_all_rectangles(coords):
+    rectangles = {}
+    for x1, y1 in coords:
+        for x2, y2 in coords:
+            if (x1, y1) == (x2, y2) or frozenset([(x1,y1),(x2,y2)]) in rectangles:
+                continue
+            key = frozenset([(x1,y1), (x2,y2)])
+            rectangles[key] = (abs(x1-x2)+1)*(abs(y1-y2)+1)
+    return sorted([(v, k) for k,v in rectangles.items()], key=lambda x: x[0], reverse=True)
+
+def part_1(input_data):
+    sorted_rectangles = get_all_rectangles(process_input_into_coords(input_data))
+    return sorted_rectangles[0][0]
+
+assert part_1(test_input) == 50
+with timer() as t:
+    print(f"Part 1: {part_1(real_input)}")
+
+print(f"time elapsed {t()}s")
+
+def build_matrix(coords):  
+    return Polygon([p for p in coords])
+                
+            
+            
+def check_points(x1,y1, x2,y2, matrix):
+    x_sign = get_sign(x1, x2)
+    y_sign = get_sign(y1, y2)
+    for i in range(x1, x2+x_sign, x_sign or 1):
+        for j in range(y1,y2+y_sign, y_sign or 1):
+            if (i,j) not in matrix:
+                return False
+    return True
+            
+    
+def part_2(input_data):
+    coords = process_input_into_coords(input_data)
+    sorted_rectangles = get_all_rectangles(coords)
+    matrix = build_matrix(coords)
+    matrix_polygon = Polygon(matrix)
+    for area, (p1,p2) in sorted_rectangles:
+        polygon = Polygon([p1, (p1[0], p2[1]), p2, (p2[0], p1[1])])
+        if polygon.within(matrix_polygon):
+            return area
+
+assert part_2(test_input) == 24
+with timer() as t:
+    print(f"Part 1: {part_2(real_input)}")
+
+print(f"time elapsed {t()}s")
